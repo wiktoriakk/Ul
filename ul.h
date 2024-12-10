9e685b34154f5d7cc46d9129f4b5e4e1f167a4a2
Wiktoria Kosek <kosek.wiktoria1@gmail.com>
2024-12-10 23:19:34 +0100

    file ul.h with struct of Bee and Beehive

---
+++ ul.h
@@ -0,0 +1,24 @@
+#ifndef UL_H
+#define UL_H
+
+#include<pthread.h>
+
+typedef struct {
+	char type;
+	int age;
+	int visits;
+} Bee;
+
+typedef struct {
+	Bee* bees;
+	int total_bees;
+	int max_population;
+	int queen_lifespan;
+	int worker_lifespan;
+	int max_bees_in_hive;
+	int bees_in_hive;
+	pthread_mutex_t lock;
+	int queen_alive;
+} Beehive;
+
+#endif
