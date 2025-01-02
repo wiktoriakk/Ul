4d2849bccf32221882906d1d0affb4c065bc126c
wiktoriakk <kosek.wiktoria1@gmail.com>
2025-01-02 18:38:56 +0100

    Added overheating for bees in hive.

--- worker.c
+++ worker.c
@@ -1,14 +1,21 @@
 #include<stdio.h>
 #include<pthread.h>
 #include<unistd.h>
-#include"main.c"
+#include"worker.c"
 
 void* worker_thread(void* arg) {
 	Beehive* hive = (Beehive*)arg;
 	while(hive->queen_alive) {
 		sem_wait(&hive->entrance1);
-
+		
+		int bees_in_hive = 0;
         	for (int i = 0; i < hive->total_bees; i++) {
+			bees_in_hive++;
+			if(bees_in_hive>hive->max_bees_in_hive) {
+				printf("Przegrzanie ula! Liczba pszczol: %d\n", bees_in_hive);
+				break;
+			}
+		}
             		hive->bees[i].age++;
             		if (hive->bees[i].age > 10) {
 				printf("Pszczola %d umarla z powodu starosci.\n", i);
